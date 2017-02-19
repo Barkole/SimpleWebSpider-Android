da8cb82b44e865f71545ea992442dd58ce7c52ef
Mike <Mike@local.local>
2017-02-19 12:05:37 +0100

    Spread threads more according to throttle value and also defining a minimum relaxing time

--- app/src/main/java/com/android/webcrawler/WebCrawler.java
+++ app/src/main/java/com/android/webcrawler/WebCrawler.java
@@ -33,6 +33,8 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class WebCrawler {
 
+    private static final long MIN_THREAD_SPAWN_WAIT = 100L;
+
     private final Context mContext;
     private final SimpleHostThrottler hostThrottler;
     private final DbHelperFactory dbHelperFactory;
@@ -104,7 +106,7 @@ public class WebCrawler {
                     Log.e(Constant.TAG, "Failed to prefill database", e);
                 }
 
-                mHandler.sendEmptyMessageDelayed(Constant.MSG_SPAWN_CRAWLERS, 100);
+                mHandler.sendEmptyMessageDelayed(Constant.MSG_SPAWN_CRAWLERS, MIN_THREAD_SPAWN_WAIT);
                 return null;
             }
         }.execute();
@@ -139,37 +141,59 @@ public class WebCrawler {
     private Handler mHandler = new Handler(Looper.getMainLooper()) {
         public void handleMessage(android.os.Message msg) {
             mHandler.removeMessages(Constant.MSG_SPAWN_CRAWLERS);
-            new AsyncTask<Object, Object, Object>() {
-                @Override
-                protected Object doInBackground(Object... params) {
-                    try {
-                        final DbHelper dbHelper = dbHelperFactory.buildDbHelper();
-                        final LinkDao linkDao = dbHelper.getLinkDao();
+            if (!mManager.isShuttingDown() && throttler.hasNext() && mManager.hasUnusedThreads()) {
+                new AsyncTask<Object, Object, Object>() {
+                    @Override
+                    protected Object doInBackground(Object... params) {
+                        try {
+                            final DbHelper dbHelper = dbHelperFactory.buildDbHelper();
+                            final LinkDao linkDao = dbHelper.getLinkDao();
 
-                        if (!mManager.isShuttingDown() && throttler.hasNext() && mManager.hasUnusedThreads() && !linkDao.isQueueEmpty()) {
-                            String url = linkDao.removeNextAndCommit();
-                            if (throttler.next()) {
-                                enqueueTask(url);
-                            } else {
-                                linkDao.saveForced(url);
+                            if (!mManager.isShuttingDown() && throttler.hasNext() && mManager.hasUnusedThreads() && !linkDao.isQueueEmpty()) {
+                                String url = linkDao.removeNextAndCommit();
+                                if (throttler.next()) {
+                                    enqueueTask(url);
+                                } else {
+                                    linkDao.saveForced(url);
+                                }
                             }
+                            dbHelper.close();
+                        } catch (SQLException e) {
+                            Log.wtf(Constant.TAG, "Failed to access database", e);
                         }
-                        dbHelper.close();
-                    } catch (SQLException e) {
-                        Log.wtf(Constant.TAG, "Failed to access database", e);
-                    }
 
-                    if (mManager.isShuttingDown()) {
-                        // Quit if manager is shutting done
-                        mHandler.removeMessages(Constant.MSG_SPAWN_CRAWLERS);
+                        if (mManager.isShuttingDown()) {
+                            // Quit if manager is shutting done
+                            mHandler.removeMessages(Constant.MSG_SPAWN_CRAWLERS);
+                            return null;
+                        }
+
+                        mHandler.sendEmptyMessageDelayed(Constant.MSG_SPAWN_CRAWLERS, calcWaitTime());
                         return null;
                     }
-
-                    mHandler.sendEmptyMessageDelayed(Constant.MSG_SPAWN_CRAWLERS, 100);
-                    return null;
+                }.execute();
+            } else {
+                long waitTime = throttler.waitTime();
+                if (waitTime < MIN_THREAD_SPAWN_WAIT) {
+                    waitTime = MIN_THREAD_SPAWN_WAIT;
                 }
-            }.execute();
+                mHandler.sendEmptyMessageDelayed(Constant.MSG_SPAWN_CRAWLERS, waitTime);
+            }
         }
+
+       private long calcWaitTime() {
+           long staticWaitTime = throttler.getStaticWaitTime();
+           long waitTime = throttler.waitTime();
+           if (waitTime < staticWaitTime) {
+               waitTime = (waitTime + staticWaitTime)/2L;
+           }
+           if (waitTime < MIN_THREAD_SPAWN_WAIT) {
+               waitTime = MIN_THREAD_SPAWN_WAIT;
+           }
+
+           return waitTime;
+       }
+
 
         private void enqueueTask(String url) {
            final LinkExtractor extractor = new StreamExtractor(configuration);
